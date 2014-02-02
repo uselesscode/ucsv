cf34eefa3882a6cfb33e0cd31a20e9ed478703c4
uselesscode <i-stare@uselesscode.org>
2014-02-01 22:26:15 -0800

    Added test for chomp

--- test/internal.js
+++ test/internal.js
@@ -1,5 +1,16 @@
+/*globals chomp */
+
 test("hello internal test", function () {
   ok(1 === 1, "Passed!");
 });
 
-// No internal-only tests yet
+
+test('chomp with no new line', function () {
+  var chompped = chomp('foo\nbar');
+  strictEqual('foo\nbar', chompped);
+});
+
+test('chomp with new line', function () {
+  var chompped = chomp('foo\nbar\n');
+  strictEqual('foo\nbar', chompped);
+});
