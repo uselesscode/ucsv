6a1de602a4793d8baa4424d53c052ebc97378c4f
uselesscode <i-stare@uselesscode.org>
2014-02-12 00:18:05 -0800

    Added objectToCsv and csvToObject to the CommonJS exports.

--- source/outro.js
+++ source/outro.js
@@ -1,10 +1,9 @@
-
   // Add support for use as a CommonJS module.
-  // This allows use as a library with the Mozilla Add-On SDK
-  // or a module in Node.js via a call to require().
   if (typeof exports === "object") {
     exports.arrayToCsv = CSV.arrayToCsv;
     exports.csvToArray = CSV.csvToArray;
+    exports.objectToCsv = CSV.objectToCsv;
+    exports.csvToObject = CSV.csvToObject;
   }
 
   return CSV;
